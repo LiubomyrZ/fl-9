e312f409d4c191819f862ef1e8c82a5a48011f53
LiubomyrZ <liybkoz@gmail.com>
2018-08-19 23:17:59 +0300

    limitation added

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -2,7 +2,7 @@ var amount = parseFloat(prompt('amount of money'));
 let discount = parseFloat(prompt('discount'));
 let seyved = amount * discount / 100;
 
-if (amount <=0 || isNaN(amount) || discount<0 || isNaN(discount) || discount % 1 !== 0){
+if (amount <=0 || isNaN(amount) || discount<0 || isNaN(discount) || discount % 1 !== 0 || discount >=100){
     alert('invalid data');
 } else if (discount === 0) {
     console.log(`Price without discount: ${amount} 
